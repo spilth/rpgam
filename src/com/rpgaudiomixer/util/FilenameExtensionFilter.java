ae613e0c5b0716d81984aa78851b04469710df20
brian <brian@39520d00-d451-0410-9ebc-e784a4e5e25c>
2009-07-23 17:48:28 +0000

    Refactored FilenameExtensionFilter and added tests for it
    git-svn-id: http://spilth.unfuddle.com/svn/spilth_rpgam/trunk@102 39520d00-d451-0410-9ebc-e784a4e5e25c

--- src/com/rpgaudiomixer/util/FilenameExtensionFilter.java
+++ src/com/rpgaudiomixer/util/FilenameExtensionFilter.java
@@ -5,80 +5,31 @@ import java.io.FilenameFilter;
 import java.util.HashSet;
 import java.util.Iterator;
 
-/**
- * A class that filers filenames based on their extension.
- * 
- * @author Brian Kelly
- *
- */
-
 public final class FilenameExtensionFilter implements FilenameFilter {
-	/** The list of acceptable extensions. */
-	private HashSet<String> extensions = new HashSet<String>();
 
-	/** Default constructor. */
-	public FilenameExtensionFilter() { }
+	private HashSet<String> acceptableExtensions = new HashSet<String>();
 	
-	/**
-	 * Create a filter with a single extension.
-	 * 
-	 * @param ext The extension to use.  Should NOT include the leading dot.
-	 * "mp3", not "*.mp3" or ".mp3".
-	 *  
-	 */
-
 	public FilenameExtensionFilter(final String ext) {
-		this();
 		this.addExtension(ext);
 	}
 
-	/**
-	 * Creates a filter from an array of extensions.
-	 * 
-	 * @param extensionArray An array of extensions.
-	 */
 	public FilenameExtensionFilter(final String[] extensionArray) {
-		// Loops through array and add extensions
 		for (int i = 0; i < extensionArray.length; i++) {
 			this.addExtension(extensionArray[i]);
 		}
 	}
 
-	/** 
-	 * @param extension The extension to add.
-	 */
 	public void addExtension(final String extension) {
-		extensions.add(extension);
+		acceptableExtensions.add(extension.toLowerCase());
 	}
 	
-	/**
-	 * @param extension The extension to remove.
-	 */
-	public void removeExtension(final String extension) {
-		extensions.remove(extension);
-	}
-	
-	/**
-	 * FilenameFilter implementation.
-	 * Accepts a file if it ends with any of the extensions
-	 * that this filter contains.
-	 * 
-	 * @param file The file to test.
-	 * @param string I'm not sure... ???
-	 * 
-	 * @return True if the file ends with one of the supplied extensions.
-	 */
-	public boolean accept(final File file, final String string) {
-		if (extensions.isEmpty()) {
-			return false;
-		}
-
-		for (Iterator<String> i = extensions.iterator(); i.hasNext();) {
-			if (string.toLowerCase().endsWith("." + (String) i.next())) {
-				return !(new File(string)).isHidden();
+	public boolean accept(final File dir, final String filename) {
+		for (Iterator<String> i = acceptableExtensions.iterator(); i.hasNext();) {			
+			String suffix = "." + (String) i.next();
+			if (filename.toLowerCase().endsWith(suffix)) {
+				return true;
 			}
 		}
-
 		return false;
 	}
 
---
+++ test/FilenameExtensionFilterTest.java
@@ -0,0 +1,26 @@
+import java.io.File;
+
+import com.rpgaudiomixer.util.FilenameExtensionFilter;
+
+import junit.framework.TestCase;
+
+public class FilenameExtensionFilterTest extends TestCase {
+
+	private static final File DIR = new File(".");
+	
+	public void testSingleExtension() {
+		assertTrue(new FilenameExtensionFilter("bmp").accept(DIR, "foo.bmp"));
+	}
+	
+	public void testMultipleExtensions() {
+		FilenameExtensionFilter filter = new FilenameExtensionFilter(new String[] {"bmp", "jpg"});
+		assertTrue(filter.accept(DIR, "foo.bmp"));
+		assertTrue(filter.accept(DIR, "foo.jpg"));
+	}
+
+	public void testCaseSensitiveExtension() {
+		assertTrue(new FilenameExtensionFilter("BMP").accept(DIR, "foo.bmp"));
+		assertTrue(new FilenameExtensionFilter("bmp").accept(DIR, "foo.BMP"));
+	}
+
+}
